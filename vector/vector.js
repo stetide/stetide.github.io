5ede35f74663141f0616a643a7d32386c5cf901a
Andre <ameyer@energy-consulting-meyer.de>
2021-02-15 20:52:39 +0100

    vector

---
+++ vector/vector.js
@@ -0,0 +1,61 @@
+var input = document.getElementById("txtInpt");
+var send = document.getElementById("sendBtn");
+var display = document.getElementById("display");
+
+input.addEventListener("keydown", function(event) {
+    if (event.keyCode == 13) {
+        event.preventDefault();
+        document.getElementById("sendBtn").click();
+    }
+});
+
+function clear() {
+    var pars = document.getElementsByTagName("p");
+    for (var i = pars.length -1; i >= 0; i--) {
+        pars[i].parentNode.removeChild(pars[i]);
+    }
+}
+
+function onSend() {
+    var cmdSpan = document.createElement("span");
+    var cmdSpanNode = document.createTextNode("$");
+    var cmdNode = document.createTextNode(input.value);
+    cmdSpan.appendChild(cmdSpanNode);
+    var cmdBr = document.createElement("br");
+
+    switch (input.value) {
+    case "exit":
+        location.reload();
+    case "clear":
+        input.value = "";
+        clear();
+        return;
+    case "":
+        var par = document.createElement("p");
+        par.appendChild(cmdSpan)
+        display.prepend(par);
+        return;
+    }
+    var res = run(input.value);
+    input.value = "";
+    console.log(res);
+    
+    var par = document.createElement("p");
+    var parNode = document.createTextNode(res);
+    
+    var resSpan = document.createElement("span");
+    var resNode = document.createTextNode(">>");
+    resSpan.appendChild(resNode);
+    
+    par.appendChild(cmdSpan);
+    par.appendChild(cmdNode);
+    par.appendChild(cmdBr);
+    if (res != "") {
+        par.appendChild(resSpan);
+        par.appendChild(parNode);
+    }
+
+    display.prepend(par);
+    display.scrollTop = display.scrollHeight;
+}
+send.addEventListener("click", onSend);
