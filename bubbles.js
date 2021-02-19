4c68850272226fab4927b4b0501292b76bf1c209
Andre <ameyer@energy-consulting-meyer.de>
2021-02-19 13:33:50 +0100

    main website

---
+++ bubbles.js
@@ -0,0 +1,8 @@
+var bubbles = document.getElementsByClassName("bubble");
+/*for (bubble in bubbles) {
+    var size = Math.floor(Math.random() * 10) + 20;
+    bubble.setAttribute("style", "width:")
+}*/
+for (var i = 0; i < bubbles.length; i++) {
+    bubbles[i].setA
+}
