9d929ef69f92cf24be02b82bc4b7c32273530c6c
Andre <ameyer@energy-consulting-meyer.de>
2021-02-20 00:38:07 +0100

    fixed color

--- bubbles.js
+++ bubbles.js
@@ -1,17 +1,17 @@
-var bubbles = document.getElementsByClassName("bubble");
-/*for (bubble in bubbles) {
+/*var bubbles = document.getElementsByClassName("bubble");
+for (bubble in bubbles) {
     var size = Math.floor(Math.random() * 10) + 20;
     bubble.setAttribute("style", "width:")
-}*/
+}
 for (var i = 0; i < bubbles.length; i++) {
     var size = Math.floor(Math.random() * 30) + 30;
     $(bubbles[i]).css({
         "width": `${size}px`,
         "height": `${size}px`,
         "opacity": Math.random() * 0.6 + 0.2,
-        // "transform": "translate("+(i*100/bubbles.length)+"vw, "+(-i*100/bubbles.length)+"vh)",
+        "transform": `translate(${65 - Math.random() * 130}vw, ${65 - Math.random() * 130}vh)`,
     });
-}
+}*/
 
 function animateBubbles() {
     for (var i = 0; i < bubbles.length; i++) {
@@ -19,5 +19,16 @@ function animateBubbles() {
     }
 }
 
+var bubbles = document.getElementsByClassName("bubble");
+for (var i = 0; i < bubbles.length; i++) {
+    var size = Math.floor(Math.random() * 30) + 30;
+    $(bubbles[i]).css({
+        "width": `${size}px`,
+        "height": `${size}px`,
+        "opacity": Math.random() * 0.6 + 0.2,
+        "transform": `translate(${65 - Math.random() * 130}vw, ${65 - Math.random() * 130}vh)`,
+    });
+}
 animateBubbles();
+setTimeout(function(){ $(".bubble").css("transition", "transform 5s linear"); animateBubbles(); }, 1);
 setInterval(animateBubbles, 5000);
