c390cb39e7fdf4ace2b784f3c90b45d6100070a4
Andre <ameyer@energy-consulting-meyer.de>
2021-02-24 20:58:28 +0100

    improved usability

--- vector/vector.js
+++ vector/vector.js
@@ -45,12 +45,12 @@ function onSend() {
     let cmdBr = document.createElement("br");
 
     // tmp
-    if (input.value == "") {
+    /*if (input.value == "") {
         let par = document.createElement("p");
         par.appendChild(cmdSpan)
         display.prepend(par);
         return;
-    }
+    }*/
     let res = run(input.value);
     input.value = "";
     console.log(res);
