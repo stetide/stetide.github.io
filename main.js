5f2bb16963329f5284d50dfce47268870174b89f
stetide <ameyer@energy-consulting-meyer.de>
2022-10-04 01:15:57 +0200

    waves

--- main.js
+++ main.js
@@ -13,26 +13,20 @@ function setColorScheme(colorScheme) {
 // theme change
 document.querySelectorAll('.circlebutton').forEach((element) => element.addEventListener('click', () => setColorScheme(element.id)));
 
+// set dark theme at the beginning if the user prefferes
+if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+    setColorScheme('dark');
+}
+
+// watch for theme changes
+window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', event => {
+    const newColorScheme = event.matches ? "dark" : "light";
+    setColorScheme(newColorScheme);
+});
+
 // card switch
 document.querySelectorAll('.card').forEach(element => element.addEventListener('click', () => {
     document.querySelectorAll('.card').forEach(e => () => {e.style.zIndex = '2';});
     element.style.zIndex = '1';
     console.log(element.style.zIndex);
 }));
-
-// snapp pages
-let currentPage = 1;
-let previousY = 0;
-
-window.addEventListener('scroll', (e) => {
-    e.preventDefault();
-    console.log(window.scrollY);
-    // if (window.scrollY > previousY && currentPage < 3) {
-    //     currentPage++;
-    // } else if (window.scrollY < previousY && currentPage > 1) {
-    //     currentPage--;
-    // }
-    // previousY = window.scrollY; 
-    // console.log(currentPage);
-    // document.querySelector('.page'+currentPage.toString()).scrollIntoView();
-}, false);
